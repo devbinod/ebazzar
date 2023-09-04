9e90686db60bd638f46c7ec77a7bf226eab82274
devbinod <binodpant.nep@gmail.com>
2023-09-03 20:59:14 -0400

    initial commit

---
+++ src/main/java/edu/miu/cs/cs425/elibrary/ELibrary/Demo/ELibraryDemoApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs425.elibrary.ELibrary.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELibraryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELibraryDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/miu/cs/cs425/elibrary/ELibrary/Demo/ELibraryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs425.elibrary.ELibrary.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELibraryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
